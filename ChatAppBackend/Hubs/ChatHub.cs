7e8cc446a7477d9278db7725de78bd4b5a96b0d3
c0mbined <74930812+c0mbined@users.noreply.github.com>
2023-09-26 12:58:56 +0100

    Initial commit with ChatAppFrontend and ChatAppBackend

---
+++ ChatAppBackend/Hubs/ChatHub.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading.Tasks;
+
+public class ChatHub : Hub
+{
+    private readonly ILogger<ChatHub> _logger;
+
+    public ChatHub(ILogger<ChatHub> logger)
+    {
+        _logger = logger;
+    }
+
+    public async Task SendMessage(string user, string message)
+    {
+        // Get the current timestamp
+        var timestamp = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+
+        // Format the message for the console
+        var formattedMessage = $"{timestamp} - {user}: {message}";
+
+        // Log the formatted message
+        _logger.LogInformation(formattedMessage);
+
+        // Broadcast the message to all clients
+        await Clients.All.SendAsync("ReceiveMessage", user, message);
+    }
+
+    public void NotifyBackendOfConnection(string message)
+    {
+        Console.WriteLine(message);
+    }
+}
